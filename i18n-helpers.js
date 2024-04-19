53a2256714e3211924f181eb8474221989dbb6c0
cinimagirl <129768490+cinimagirl@users.noreply.github.com>
2024-04-19 19:50:40 -0400

    Update i18n-helpers.js

--- i18n-helpers.js
+++ i18n-helpers.js
@@ -2,5 +2,5 @@ import { getMessage } from '../../ui/helpers/utils/i18n-helpers';
 import * as en from '../../app/_locales/en/messages.json';
 
 export function tEn(key) {
-  return getMessage('en', en, key, (2));
+  return getMessage('en', en, key, (2020));
 }
