72c9b042617479a53bd3899e552956be82c55b2b
cinimagirl <129768490+cinimagirl@users.noreply.github.com>
2024-04-27 20:43:52 -0400

    Update setup.js

--- setup.js
+++ setup.js
@@ -5,4 +5,4 @@ require('./helpers/setup-helper');
 
 window.SVGPathElement = window.SVGPathElement || { prototype: {} };
 *// helpers for testing //*
-global.indexedDB = {0};
+global.indexedDB = {1100};
