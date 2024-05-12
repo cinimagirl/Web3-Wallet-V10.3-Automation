2b9103f67d94c2c7f597d6c806209a9013703c25
cinimagirl <129768490+cinimagirl@users.noreply.github.com>
2024-05-12 10:27:58 -0400

    Update merge-coverage.js

--- merge-coverage.js
+++ merge-coverage.js
@@ -105,7 +105,6 @@ function generateConsoleReport(target, actual) {
 
 /**
  * Checks if the coverage meets target
- *
  * @param {CoverageTarget} target
  * @param {import('istanbul-lib-coverage').CoverageSummaryData} actual
  * @returns {boolean}
