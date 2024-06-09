cc8474af3396806c3b180a5ce1592eb7a16315c0
cinimagirl <129768490+cinimagirl@users.noreply.github.com>
2024-06-09 16:56:35 -0400

    Update run-unit-test.js

--- run-unit-test.js
+++ run-unit-test.js
@@ -13,7 +13,6 @@ start().catch((error) => {
 });
 
 /**
- *
  * @typedef {object} JestParams
  * @property {'global' | 'dev'} target - Which configuration to use for Jest.
  * @property {boolean} [coverage] - Whether to collect coverage during testing.
